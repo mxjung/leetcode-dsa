f8c2de9ef893b2ecb39a9b57b766808614916d8f
Max Jung <mjung@Peters-MacBook-Pro.local>
2020-08-20 00:35:47 -0700

    completed exercise 26

---
+++ 26_Remove_Duplicates_from_Sorted_Array.js
@@ -0,0 +1,16 @@
+var removeDuplicates = function(nums) {
+  let i = 0;
+
+  if (nums.length <= 1) {
+      return nums.length;
+  }
+
+  for (let j = 0; j < nums.length; j++) {
+      if (nums[i] !== nums[j]) {
+          i++;
+          nums[i] = nums[j];
+      }
+  }
+  console.log(nums);
+  return i + 1;
+};
