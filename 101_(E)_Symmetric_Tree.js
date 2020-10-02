194bda3f59aaa3f8674288e23865d9db34f2a564
Max Jung <mjung@Peters-MacBook-Pro.local>
2020-10-01 22:39:22 -0700

    Completed 101 and 448, changed titles to indicate difficulty of problem

---
+++ 101_(E)_Symmetric_Tree.js
@@ -0,0 +1,55 @@
+/**
+
+  Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+  For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+  Solution: https://leetcode.com/problems/symmetric-tree/discuss/33073/JavaScript-recursive-and-iterative-solutions
+ */
+
+// Recursive
+var isSymmetric = function(root) {
+  if (!root) { // Sanity check
+      return true;
+  }
+
+  // Check if tree s & t are mirroring each other
+  function isMirror(s, t) {
+      if (!s && !t) {
+          return true; // Both nodes are null, ok
+      }
+      if (!s || !t || s.val !== t.val) {
+          return false; // Found a mismatch
+      }
+      // Compare the left subtree of `s` with the right subtree of `t`
+      // and the right subtree of `s` with the left subtree of `t`
+      return isMirror(s.left, t.right) && isMirror(s.right, t.left);
+  }
+
+  return isMirror(root.left, root.right);
+};
+
+
+// Iterative (DFS)
+function isMirror(p, q) {
+  // Create two stacks
+  var s1 = [p], s2 = [q];
+
+  // Perform preorder traversal
+  while (s1.length > 0 || s2.length > 0) {
+      var n1 = s1.pop(), n2 = s2.pop();
+
+      // Two null nodes, let's continue
+      if (!n1 && !n2) continue;
+
+      // Return false as long as there is a mismatch
+      if (!n1 || !n2 || n1.val !== n2.val) return false;
+
+      // Scan tree s from left to right
+      // and scan tree t from right to left
+      s1.push(n1.left); s1.push(n1.right);
+      s2.push(n2.right); s2.push(n2.left);
+  }
+
+  return true;
+}
---
+++ 448_(E)_Find_All_Numbers_Disappeared.js
@@ -0,0 +1,51 @@
+/**
+
+  Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+  Find all the elements of [1, n] inclusive that do not appear in this array.
+
+  Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+
+ */
+
+ // Brute Force Method
+ var findDisappearedNumbers = function(nums) {
+  let numFreq = {};
+  let returnArr = [];
+
+  for (let num of nums) {
+      if (!(num in numFreq)) {
+          numFreq[num] = 1;
+      }
+  }
+
+  for (let i=1; i<=nums.length; i++) {
+      if (!(i in numFreq)) {
+          returnArr.push(i);
+      }
+  }
+
+  console.log(numFreq);
+  return returnArr;
+};
+
+// O(N) solution
+// Involves keeping track of index, and finding out which number in that index is not negative
+var findDisappearedNumbers = function(nums) {
+  var actual,
+      i,
+      res = [];
+
+  for(i = 0; i < nums.length; i++){
+      actual = Math.abs(nums[i]) - 1;
+      if(nums[actual] > 0)
+          nums[actual] *= -1;
+  }
+
+  for(i = 0; i < nums.length; i++){
+      if(nums[i] > 0)
+          res.push(i+1);
+  }
+
+  return res;
+};
