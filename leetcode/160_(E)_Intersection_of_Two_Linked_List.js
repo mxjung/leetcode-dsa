1f0e029502e19f6d300592c8fc513fa90f6138b7
Max Jung <mjung@Peters-MacBook-Pro.local>
2020-12-03 22:24:16 -0800

    Adding comments to problems

---
+++ leetcode/160_(E)_Intersection_of_Two_Linked_List.js
@@ -0,0 +1,34 @@
+/**
+Write a program to find the node at which the intersection of two singly linked lists begins.
+
+Topic: Two pointers
+ *
+ * 12/03/2020
+ */
+
+var getIntersectionNode = function (headA, headB) {
+  if (headA === null || headB === null) return null;
+
+  let pointerA = headA;
+  let pointerB = headB;
+
+  while (pointerA !== pointerB) {
+    if (pointerA === null) {
+      pointerA = headB;
+    } else {
+      pointerA = pointerA.next;
+    }
+
+    if (pointerB === null) {
+      pointerB = headA;
+    } else {
+      pointerB = pointerB.next;
+    }
+  }
+
+  // will be node value or null (depending on when pointerA === pointerB)
+  return pointerA;
+};
+
+// Link to explanation
+// https://leetcode.com/problems/intersection-of-two-linked-lists/discuss/49785/Java-solution-without-knowing-the-difference-in-len!
